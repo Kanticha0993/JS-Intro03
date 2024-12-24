e8997e02313d087029b8ffde7f8aa36ac47623c5
Kanticha0993 <s6606021610010@email.kmutnb.ac.th.>
2024-12-24 11:38:26 +0700

    975

--- iterator1.js
+++ iterator1.js
@@ -1,12 +1,12 @@
-let str = "Hello";
+//let str = "Hello";
 
 //does the same as
-// for (let char of str) console.log(char);
+for (let char of "test") {console.log(char);}
 
-let iterator = str[Symbol.iterator]();
+//let iterator = str[Symbol.iterator]();
 
-while (true) {
-    let result = iterator.next();
-    if (result.done) break;
-    console.log(result.value); //outputs characters one by one
-}
+//while (true) {
+    //let result = iterator.next();
+    //if (result.done) break;
+    //console.log(result.value); //outputs characters one by one
+//}
--- test2.js
+++
@@ -1,8 +0,0 @@
-let furniture = ['Table', 'Chairs', 'Couch'];
-
-for (let item of furniture) {
-  console.log(`Letters in "${item}":`);
-  for (let letter of item) {
-    console.log(letter);
-  }
-}
